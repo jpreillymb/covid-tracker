cbcf0d44be42713fa3b015b2fdb774b90eff5a51
JP Reilly <jpreilly@stanford.edu>
2020-05-07 00:28:14 -0700

    Updates

---
+++ precache-manifest.a54e5cfbc9ffddd7201e73ba5e517dee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19dbd71833cd5baca5b85c2677b3a8e3",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "6b3723dcb33e5159e13b",
+    "url": "/covid-tracker/static/css/main.cfeb1129.chunk.css"
+  },
+  {
+    "revision": "f08384e2c61fa07d5ec4",
+    "url": "/covid-tracker/static/js/2.e2652ebf.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid-tracker/static/js/2.e2652ebf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b3723dcb33e5159e13b",
+    "url": "/covid-tracker/static/js/main.9ac93cea.chunk.js"
+  },
+  {
+    "revision": "1ee3619b46a3284fdd0e",
+    "url": "/covid-tracker/static/js/runtime-main.d20c9eb4.js"
+  }
+]);
